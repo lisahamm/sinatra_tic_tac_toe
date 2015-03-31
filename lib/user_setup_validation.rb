6f1351a55e42eae32cce1f2b5e873c32444ebf48
Lisa Hamm <lisalhamm@gmail.com>
2015-03-31 15:26:29 -0500

    Created UserSetupValidation class to validate the users setup input

---
+++ lib/user_setup_validation.rb
@@ -0,0 +1,50 @@
+module UserSetupValidation
+    attr_reader :errors
+
+  def initialize(setup)
+    @setup = setup
+    @errors = {}
+  end
+
+  def valid?
+    run_validations
+    errors.empty?
+  end
+
+  def invalid?
+    !valid?
+  end
+
+  private
+
+  def run_validations
+    validate(:player_mark)
+    validate(:computer_opponent)
+    validate(:player_order)
+  end
+
+  def validate(key)
+    if valid_options[key].include?(@setup[key])
+      true
+    else
+      self.errors[key] = message_options[key]
+      false
+    end
+  end
+
+  def valid_options
+    {
+      player_mark: ["X", "O"],
+      computer_opponent: ["yes", "no"],
+      player_order: ["first", "second"]
+    }
+  end
+
+  def message_options
+    {
+      player_mark: "Please select a mark to continue",
+      computer_opponent: "Please indicate if you would like to play against the computer",
+      player_order: "Please select if you would like to go first or second"
+    }
+  end
+end
