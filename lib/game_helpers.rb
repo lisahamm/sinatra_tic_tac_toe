f717a21ff461839f18afc42efb0e15f55d0af74f
Lisa Hamm <lisalhamm@gmail.com>
2015-04-01 09:45:04 -0500

    Added method to serialize game data to store in database

--- lib/game_helpers.rb
+++ lib/game_helpers.rb
@@ -7,6 +7,14 @@ module GameHelpers
     end
   end
 
+  def serialize_game_data(game, time)
+    {:player1_mark => game.player1_mark,
+     :player2_mark => game.player2_mark,
+     :computer_player_mark => game.ai_mark ,
+     :moves => moves_to_string(game.board_to_array),
+     :time => time}
+  end
+
   def array_to_board(array)
     TicTacToe::Board.new(cells: array)
   end
