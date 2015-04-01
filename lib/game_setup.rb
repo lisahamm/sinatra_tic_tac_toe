862f1eb56ba3ce12931d034c72db6f6debf34eb7
Lisa Hamm <lisalhamm@gmail.com>
2015-04-01 15:35:37 -0500

    Refactored GameSetup to be initialized with player_mark_options. #configure_specifications now returns options hash

--- lib/game_setup.rb
+++ lib/game_setup.rb
@@ -1,23 +1,25 @@
 require 'tic_tac_toe'
 
 class GameSetup
-  attr_accessor :user_input, :rules, :options
+  attr_accessor :user_input, :rules, :options, :player_mark_options
 
-  def initialize(params)
+  def initialize(params, player_mark_options=['X','O'])
     @user_input = params
-    @rules = TicTacToe::Rules.new
+    @player_mark_options = player_mark_options
   end
 
-  def configure_specifications!
-    @options = {}
+  def configure_specifications
+    options = {}
     options[:player_marks] = setup_player_marks(user_input[:player_order], user_input[:player_mark])
     options[:current_player_mark] = options[:player_marks].first
     options[:ai_mark] = setup_computer_opponent(user_input, options[:player_marks])
+    options
   end
 
-  def create_game!
-    configure_specifications!
-    TicTacToe::Game.new(options)
+  def create_game
+    game = TicTacToe::Game.new(configure_specifications)
+    check_for_computer_turn(game)
+    game
   end
 
   private
@@ -30,17 +32,19 @@ class GameSetup
     end
   end
 
-  def valid_player_marks
-    rules.player_marks
-  end
-
   def opponent_mark(player_mark)
-    valid_player_marks.select {|valid_mark| valid_mark != player_mark}.first
+    player_mark_options.select {|valid_mark| valid_mark != player_mark}.first
   end
 
   def setup_computer_opponent(user_input, player_marks)
     if user_input[:computer_opponent] == "yes"
       user_input[:player_order] == "first" ? player_marks[1] : player_marks[0]
+    end
+  end
+
+  def check_for_computer_turn(game)
+    if game.current_player_mark == game.ai_mark
+      game.take_turn(game.generate_ai_move)
     end
   end
 
