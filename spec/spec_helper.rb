2b16fc6b16551f7b46772f2d185f1763e999b4c5
Lisa Hamm <lisalhamm@gmail.com>
2015-04-02 14:58:32 -0500

    Configured DatabaseCleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,21 @@ $:.unshift(lib) unless $:.include?(lib)
 $:.unshift(spec) unless $:.include?(spec)
 
 
-RSpec.configure do |conf|
-  conf.include Rack::Test::Methods
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner[:sequel, {:connection => Sequel.connect('postgres://localhost/tictactoe_test')}]
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
