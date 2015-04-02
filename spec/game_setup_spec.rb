65b56d45cf3ac769a551983f246caa6ba9d20a8d
Lisa Hamm <lisalhamm@gmail.com>
2015-04-02 15:02:55 -0500

    Removed ! on #create_game

--- spec/game_setup_spec.rb
+++ spec/game_setup_spec.rb
@@ -4,19 +4,12 @@ require 'game_setup'
 describe 'GameSetup' do
   params = {:player_mark => "X", :computer_opponent => "yes", :player_order => "first"}
 
-  it "configures the setup options" do
-    setup = GameSetup.new(params)
-    setup.configure_specifications!
-    expect(setup.options[:player_marks]).to eq ["X", "O"]
-    expect(setup.options[:current_player_mark]).to eq "X"
-    expect(setup.options[:ai_mark]).to eq "O"
-  end
-
   it "initializes the game" do
     setup = GameSetup.new(params)
-    setup.configure_specifications!
-    game = setup.create_game!
+    game = setup.create_game
     expect(game.nil?).to eq false
+    expect(game.player_marks).to eq ["X", "O"]
+    expect(game.current_player_mark).to eq "X"
     expect(game.ai_mark).to eq "O"
   end
 end
