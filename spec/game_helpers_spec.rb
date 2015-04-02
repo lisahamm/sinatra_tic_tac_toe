43f912e4f51ba3932b8fafc03a1f2868ca5ebefe
Lisa Hamm <lisalhamm@gmail.com>
2015-04-02 15:01:47 -0500

    Removed tests for methods that no longer exist

--- spec/game_helpers_spec.rb
+++ spec/game_helpers_spec.rb
@@ -4,17 +4,4 @@ require 'game_helpers'
 describe 'GameHelpers' do
   include GameHelpers
 
-  it "executes computer's move if it is the computer's turn" do
-    options = {player_marks: ["X", "O"], current_player_mark: "O", ai_mark: "O"}
-    game = TicTacToe::Game.new(options)
-    check_for_computer_turn(game)
-    expect(game.board.empty?).to eq false
-  end
-
-  it "does not update the board if it is not the computer's turn" do
-    options = {player_marks: ["X", "O"], current_player_mark: "X", ai_mark: "O"}
-    game = TicTacToe::Game.new(options)
-    check_for_computer_turn(game)
-    expect(game.board.empty?).to eq true
-  end
 end
